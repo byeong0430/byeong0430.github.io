d965dcc7edb40aa9c3aff61cdc59985137744d61
Byeong Kim <byeong.kim0430@gmail.com>
2018-07-05 20:27:37 -0700

    added a function that displays nav icon description

--- js/main.js
+++ js/main.js
@@ -1,17 +1,4 @@
-$(document).ready(function () {
-
-  // display button description on hover
-  $('ul.navbar-nav a.nav-link').on('mouseenter', function () {
-    const thisHref = $(this).attr('href');
-    const navDescription = thisHref.replace('#', '');
-    const capitaliseFirstLetter = name => name.toLowerCase().replace(/^\w/, letter => letter.toUpperCase());
-
-    $('#nav-description').show();
-    $('#nav-description').attr('href', thisHref);
-    $('#nav-description').text(capitaliseFirstLetter(navDescription));
-  })
-
-  $('ul.navbar-nav a.nav-link').on('mouseleave', function () {
-    $('#nav-description').text('');
-  })
+$(() => {
+  // show nav icon description on mouse enter
+  showNavDescription(100);
 });
