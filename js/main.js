f9115c2f2186834bafcf3f962f8a831db872810c
Byeong Kim <byeong.kim0430@gmail.com>
2018-07-05 19:24:17 -0700

    display icon description on the left side of navbar

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
-// $(document).ready(function(){
+$(document).ready(function () {
 
+  // display button description on hover
+  $('ul.navbar-nav a.nav-link').on('mouseenter', function () {
+    const thisHref = $(this).attr('href');
+    const navDescription = thisHref.replace('#', '');
+    const capitaliseFirstLetter = name => name.toLowerCase().replace(/^\w/, letter => letter.toUpperCase());
 
+    $('#nav-description').show();
+    $('#nav-description').attr('href', thisHref);
+    $('#nav-description').text(capitaliseFirstLetter(navDescription));
+  })
 
-// });
+  $('ul.navbar-nav a.nav-link').on('mouseleave', function () {
+    $('#nav-description').text('');
+  })
+});
